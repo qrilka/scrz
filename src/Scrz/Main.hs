c4672475e38bdc032b3539e31d8139b1d1d2e921
Tomas Carnecky <tomas.carnecky@gmail.com>
2013-07-07 07:26:37 +0000

    ps as alias for list-containers

--- src/Scrz/Main.hs
+++ src/Scrz/Main.hs
@@ -1,8 +1,6 @@
 module Main where
 
-import Data.Maybe (isJust)
 import qualified Data.Map as M
-import qualified Data.List as L
 import qualified Data.Aeson as A
 import qualified Data.ByteString.Lazy as LBS
 
@@ -11,16 +9,12 @@ import Control.Monad
 import System.Environment
 import System.Directory
 import System.Posix.Process
-import System.Posix.Files
 import System.Posix.IO
 import System.Posix.Terminal (openPseudoTerminal, getSlaveTerminalName)
-import System.IO
 
 import Control.Exception
 import Control.Concurrent
 import Control.Concurrent.STM
-import Control.Concurrent.STM.TVar
-import Control.Concurrent.STM.TMVar
 
 import Network.Socket
 
@@ -29,7 +23,6 @@ import Scrz.Types
 import Scrz.Network
 import Scrz.Container
 import Scrz.Image
-import Scrz.Service
 import Scrz.Socket
 import Scrz.Signal
 import Scrz.Commands
@@ -51,7 +44,7 @@ createControlThread mvar runtime = do
 
     cleanup :: Socket -> Either SomeException () -> IO ()
     cleanup socket ex = do
-        putStrLn $ show ex
+        logger $ show ex
         close socket
         removeFile controlSocketPath
         atomically $ putTMVar mvar ()
@@ -61,13 +54,11 @@ createControlThread mvar runtime = do
         rt <- atomically $ readTVar runtime
         exists <- hasContainer rt Local service
 
-        if exists
-            then return ()
-            else do
-                container <- createContainer runtime Local service
-                startContainer runtime container Nothing
-                id <- atomically $ containerId <$> readTVar container
-                logger $ show id
+        unless exists $ do
+            container <- createContainer runtime Local service
+            startContainer runtime container Nothing
+            id <- atomically $ containerId <$> readTVar container
+            logger $ show id
 
     loadLocalConfig = do
         conf <- LBS.readFile "/etc/scrz/config.json"
@@ -118,6 +109,10 @@ run [ "list-containers" ] = do
     sendCommand ListContainers >>= printResponse
     return ()
 
+run [ "ps" ] = do
+    sendCommand ListContainers >>= printResponse
+    return ()
+
 run [ "stop-container", id ] = do
     sendCommand $ StopContainer id
     return ()
@@ -144,7 +139,8 @@ run [ "console", id ] = do
 run [ "list-images" ] = do
     images <- loadImages
     forM_ (M.toList images) $ \(id, image) -> do
-        putStrLn $ imageId image
+        when ('.' /= head (imageId image)) $ do
+            putStrLn $ imageId image
 
 run [ "pack-image", id ] = do
     image <- getImage id
@@ -152,7 +148,6 @@ run [ "pack-image", id ] = do
 
 run ("run":args) = do
     let ra = parseRunArguments (RunArgs "" [] [] Nothing) args
-    putStrLn $ show ra
 
     (ptm, pts) <- openPseudoTerminal
     slaveName <- getSlaveTerminalName ptm
